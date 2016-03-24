e324c5ba82c8e405e0f6b3287a69cb6af21b5535
JatanX <r.perag@gmail.com>
2016-03-24 13:36:38 +0100

    KoenCode

--- code/test3.vhd
+++ code/test3.vhd
@@ -1,3 +1,4 @@
+
 library IEEE;
 use IEEE . STD_LOGIC_1164 .ALL;
 use IEEE . NUMERIC_STD .ALL;
@@ -8,8 +9,8 @@ use IEEE . NUMERIC_STD .ALL;
 entity s88 is
 	port
 	(
-		OnboardClock : in std_logic;
-		LED0 : out std_logic
+		OnboardClock, HighBit, DataOut : in std_logic;
+		LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7, Clock : out std_logic
 	);
 end s88;
 
@@ -20,7 +21,7 @@ architecture s88Timing of s88 is
 		timer : process(OnboardClock)
 		--Variabelen
 		variable ClockCounter : unsigned (24 downto 0) := (others => '0');
-		variable TijdseenheidCounter : integer := 0;
+		variable TijdseenheidCounter : unsigned (7 downto 0) := (others => '0');
 		--daadwerkelijk process
 		begin
 			if(rising_edge(OnboardClock)) then
@@ -143,7 +144,9 @@ TimingCounter(0) = '0' and TimingCounter(1) = '0' and TimingCounter(2) = '0' and
 			 if(HighBit = '1') then
 			 LED7 <= DataOut;
 			 end if;
-        --niks
+
+			 when others =>
+        	--niks
       end case;
 
  				end if;
