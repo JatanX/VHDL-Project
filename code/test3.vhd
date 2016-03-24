556dc66a0b94852ee9a93584bdf092fb24ecbc49
JatanX <r.perag@gmail.com>
2016-03-24 13:43:39 +0100

    zucht

--- code/test3.vhd
+++ code/test3.vhd
@@ -10,7 +10,7 @@ entity s88 is
 	port
 	(
 		OnboardClock, HighBit, DataOut : in std_logic;
-		LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7, Clock : out std_logic
+		LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7, Clock, Load : out std_logic
 	);
 end s88;
 
@@ -31,6 +31,22 @@ architecture s88Timing of s88 is
 TimingCounter(0) = '0' and TimingCounter(1) = '0' and TimingCounter(2) = '0' and TimingCounter(3) = '0' and TimingCounter(4) = '0' and TimingCounter(5) = '0' and TimingCounter(6) = '1' and TimingCounter(7) = '0' and TimingCounter(8) = '0' and TimingCounter(9) = '0' and TimingCounter(10) = '0' and TimingCounter(11) = '1' and TimingCounter(12) = '1' and TimingCounter(13) = '1' and TimingCounter(14) = '1' and TimingCounter(15) = '0' and TimingCounter(16) = '1' and TimingCounter(17) = '0' and TimingCounter(18) = '1' and TimingCounter(19) = '1' and TimingCounter(20) = '1' and TimingCounter(21) = '1' and TimingCounter(22) = '1' and TimingCounter(23) = '0' and TimingCounter(24) = '1') then
 					TimingCounter <= "0000000000000000000000000";
 					TijdseenheidCounter := TijdseenheidCounter + 1;
+		case TijdseenheidCounter is
+			when "00000001" =>
+			Load <= '1';
+			when "00000100" =>
+			Reset <= '1';
+			when "00000101" =>
+			Reset <= '0';
+			when "00000111" =>
+			Load <= '0';
+			when others =>
+			-- niks
+		end case;
+
+
+
+
          case TijdseenheidCounter is
 			 when "00000010" =>
 			 Clock <= '1';
