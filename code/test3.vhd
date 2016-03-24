20f73be20da730b4b195073e0444a48fc1ff915f
JatanX <r.perag@gmail.com>
2016-03-24 12:09:37 +0100

    updated

--- code/test3.vhd
+++ code/test3.vhd
@@ -30,7 +30,18 @@ architecture s88Timing of s88 is
 TimingCounter(0) = '0' and TimingCounter(1) = '0' and TimingCounter(2) = '0' and TimingCounter(3) = '0' and TimingCounter(4) = '0' and TimingCounter(5) = '0' and TimingCounter(6) = '1' and TimingCounter(7) = '0' and TimingCounter(8) = '0' and TimingCounter(9) = '0' and TimingCounter(10) = '0' and TimingCounter(11) = '1' and TimingCounter(12) = '1' and TimingCounter(13) = '1' and TimingCounter(14) = '1' and TimingCounter(15) = '0' and TimingCounter(16) = '1' and TimingCounter(17) = '0' and TimingCounter(18) = '1' and TimingCounter(19) = '1' and TimingCounter(20) = '1' and TimingCounter(21) = '1' and TimingCounter(22) = '1' and TimingCounter(23) = '0' and TimingCounter(24) = '1') then
 					TimingCounter <= "0000000000000000000000000";
 					TijdseenheidCounter := TijdseenheidCounter + 1;
-LED0 <= '1';
+         case TijdseenheidCounter is
+           when 0 =>
+          LED0 <= '1';
+          when 1 =>
+          LED1 <= '1';
+          when 2 =>
+          LED2 <= '1';
+          when 3 =>
+          LED3 <= '1';
+        when others =>
+        --niks
+      end case;
 
  				end if;
  			end if;
